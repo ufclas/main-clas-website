7a4d6003c11f6af4d05b9923660624c423f654bd
marissaculbreth <50034168+marissaculbreth@users.noreply.github.com>
2021-10-06 13:16:00 -0400

    Add content for new clas layout interior page

--- public/templates/ufclas-main-website-interior.php
+++ public/templates/ufclas-main-website-interior.php
@@ -3,4 +3,31 @@
  * Template Name: CLAS Website Interior Pages
  * Template Post Type: page
  */
+
+ get_header();
+?>
+
+<?php
+	/*
+	 * Uses the Featured Image as a hero image
+	 */
+	clasHeroImage();
+?>
+
+<main id="clas-interior">
+  <div class="container">
+    <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post();
+
+          the_content();
+        }
+      }
+    ?>
+  </div>
+</main>
+
+<?php
+	get_footer();
 ?>
